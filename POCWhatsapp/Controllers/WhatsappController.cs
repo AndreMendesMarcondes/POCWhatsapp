f67e1aa405583975519c3e3d4492639a7aa782a0
André Mendes Marcondes <andre.marcondes@betalearning.com.br>
2022-07-11 15:03:21 -0300

    criação api

---
+++ POCWhatsapp/Controllers/WhatsappController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using POCWhatsapp.DTO;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace POCWhatsapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhatsappController : ControllerBase
+    {
+        private readonly HttpClient _client;
+        public WhatsappController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://graph.facebook.com");
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {Environment.GetEnvironmentVariable("Token")}");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Send(WhatsappRequestDTO whatsappRequestDTO)
+        {
+            var whatsappRequest = new WhatsappRequest(whatsappRequestDTO);
+            string jsonRequest = JsonConvert.SerializeObject(whatsappRequest);
+            StringContent body = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            var responseMessage =  await _client.PostAsync($"v13.0/{Environment.GetEnvironmentVariable("AppCode")}/messages", body);
+
+            if (responseMessage.IsSuccessStatusCode)
+                return StatusCode(201);
+            else
+            {
+                return StatusCode((int)responseMessage.StatusCode, await responseMessage.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
---
+++ POCWhatsapp/DTO/WhatsappRequestDTO.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace POCWhatsapp.DTO
+{
+    public class WhatsappRequestDTO
+    {
+        public WhatsappRequestDTO()
+        {
+            TemplateLanguage = "pt_BR";
+        }
+        public string PhoneNumber { get; set; }
+        public string TemplateName { get; set; }
+        public string TemplateLanguage { get; set; }
+    }
+
+    public class WhatsappRequest
+    {
+        public WhatsappRequest(WhatsappRequestDTO whatsappRequestDTO)
+        {
+            to = whatsappRequestDTO.PhoneNumber;
+            template = new Template(whatsappRequestDTO.TemplateName, whatsappRequestDTO.TemplateLanguage);
+        }
+        public string messaging_product { get { return "whatsapp"; } }
+        public string to { get; private set; }
+        public string type { get { return "template"; } }
+        public Template template { get; private set; }
+
+        public override string? ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+    public class Template
+    {
+        public Template(string name, string language)
+        {
+            this.language = new Language(language);
+            this.name = name;
+        }
+        public string name { get; set; }
+        public Language language { get; set; }
+    }
+
+    public class Language
+    {
+        public Language(string code)
+        {
+            this.code = code;
+        }
+        public string code { get; set; }
+    }
+}
